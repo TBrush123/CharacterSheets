24d6214e6f178739eb0912027979ecdb3405556d
Dauren Ermekbaev <daurenermekbaev@MacBook-Air-Bakbergen.local>
2023-09-19 13:04:24 +0600

    Made DELETE request for single sheet

--- backend/server.js
+++ backend/server.js
@@ -38,11 +38,25 @@ app.post("/", async (req, res) => {
 app.get("/:id", async (req, res) => {
   const id = req.params.id;
   try {
-    const character = await Character.findById(id);
-    if (character) {
-      res.status(200).json(character);
+    if (!mongoose.isValidObjectId(id)) {
+      throw { message: "No such sheet was found" };
     }
+    const character = await Character.findById(id);
+    res.status(200).json(character);
   } catch (err) {
-    res.status(404).json(err);
+    res.status(404).json({ error: err });
+  }
+});
+app.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    if (!mongoose.isValidObjectId(id)) {
+      throw { message: "No such sheet was found" };
+    }
+    const character = await Character.findByIdAndDelete(id);
+    res.status(200).json(character);
+  } catch (err) {
+    res.status(404).json({ error: err });
   }
 });
