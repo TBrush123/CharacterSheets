d7dd9e39411834818645163ff947c3c10ee113d5
Dauren Ermekbaev <daurenermekbaev@MacBook-Air-Bakbergen.local>
2023-09-19 15:03:58 +0600

    Made PATCH request, Made routes and controler

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
-
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const Character = require("./Model/Database");
+const characterRoutes = require("./Routes/characters");
+
+app.use(express.json());
+app.use("/api/characters/", characterRoutes);
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
 mongoose
   .connect(process.env.uri, {
@@ -16,47 +23,3 @@ mongoose
     })
   )
   .catch((error) => console.log(error));
-
-app.use(express.json());
-
-app.get("/", async (req, res) => {
-  try {
-    const characters = await Character.find();
-    res.status(200).json(characters);
-  } catch (err) {
-    res.status(400).json({ error: err });
-  }
-});
-app.post("/", async (req, res) => {
-  try {
-    const character = await Character.create(req.body);
-    res.status(200).json(character);
-  } catch (err) {
-    res.status(400).json({ error: err });
-  }
-});
-app.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  try {
-    if (!mongoose.isValidObjectId(id)) {
-      throw { message: "No such sheet was found" };
-    }
-    const character = await Character.findById(id);
-    res.status(200).json(character);
-  } catch (err) {
-    res.status(404).json({ error: err });
-  }
-});
-app.delete("/:id", async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    if (!mongoose.isValidObjectId(id)) {
-      throw { message: "No such sheet was found" };
-    }
-    const character = await Character.findByIdAndDelete(id);
-    res.status(200).json(character);
-  } catch (err) {
-    res.status(404).json({ error: err });
-  }
-});
