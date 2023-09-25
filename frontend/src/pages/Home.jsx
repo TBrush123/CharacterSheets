e8c7a83ad063498efbe93090b8d25be991b35d87
Dauren Ermekbaev <daurenermekbaev@MacBook-Air-Bakbergen.local>
2023-09-25 13:39:54 +0600

    Made basic Home page and added some style

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [characters, setCharacters] = useState(null);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const response = await fetch("/api/characters");
+      const json = await response.json();
+
+      if (response.ok) {
+        setCharacters(json);
+      }
+    };
+    fetchCharacters();
+  }, []);
+
   return (
     <div className="home">
-        <h1>Hello</h1>
+      {characters &&
+        characters.map((character) => {
+          return (
+            <div className="character" key={character._id}>
+              <h2>{character.name}</h2>
+              <p>Level: {character.level}</p>
+              <h4>Class: {character.class}</h4>
+              <h4>Race:{character.race}</h4>
+            </div>
+          );
+        })}
     </div>
   );
 };
